46a60d157ad7bb20ff3afc60aea747b153c3feb3
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-05-14 13:07:26 -0300

    Initial import.

---
+++ pyext2lib.pxd
@@ -0,0 +1,75 @@
+cdef extern from "ext2fs/ext2fs.h":
+	cdef struct struct_io_manager:
+		pass
+
+	cdef struct ext2fs_block_bitmap:
+		pass
+
+	cdef struct ext2_super_block:
+		int s_first_data_block
+
+	cdef struct struct_ext2_filsys:
+		ext2_super_block *super
+		int group_desc_count
+		ext2fs_block_bitmap block_map
+
+	cdef struct ext2_inode:
+		pass
+
+	cdef struct ext2_struct_inode_scan:
+		pass
+
+	ctypedef struct_ext2_filsys *ext2_filsys
+	ctypedef struct_io_manager *io_manager
+	ctypedef ext2_struct_inode_scan *ext2_inode_scan
+
+	ctypedef int ext2_ino_t
+
+	cdef io_manager unix_io_manager
+
+	int ext2fs_open(char *name, int flags, int superblock,
+					unsigned int block_size, io_manager manager,
+					ext2_filsys *ret_fs)
+
+	int ext2fs_close(ext2_filsys fs)
+	int ext2fs_flush(ext2_filsys fs)
+	int ext2fs_read_block_bitmap(ext2_filsys fs)
+	int ext2fs_read_inode_bitmap(ext2_filsys fs)
+	int ext2fs_read_bitmaps(ext2_filsys fs)
+
+	int EXT2_BLOCKS_PER_GROUP(ext2_super_block *s)
+	int ext2fs_test_bit(unsigned int nr, void *addr)
+	int ext2fs_get_block_bitmap_range(ext2fs_block_bitmap bmap,
+									int start, unsigned int num,
+									void *out)
+
+	int ext2fs_open_inode_scan(ext2_filsys fs, int buffer_blocks,
+									ext2_inode_scan *ret_scan)
+	int ext2fs_get_next_inode(ext2_inode_scan scan, ext2_ino_t *ino,
+							ext2_inode *inode)
+	void ext2fs_close_inode_scan(ext2_inode_scan scan)
+	int ext2fs_inode_scan_flags(ext2_inode_scan scan, int set_flags, int
+								clear_flags)
+
+	int EXT2_SF_SKIP_MISSING_ITABLE
+
+cdef class ExtFS:
+	# XXX - Can't be instantiated directly as that leaves self.fs as NULL,
+	# making all methods segfault. Instantiate only through open() or fix
+	# this later.
+
+	cdef ext2_filsys fs
+
+	cpdef read_block_bitmap(self)
+	cpdef read_inode_bitmap(self)
+	cpdef read_bitmaps(self)
+	cpdef flush(self)
+	cpdef close(self)
+	cpdef iterinodes(self, skip_missing_itable = ?)
+
+cdef class ExtFSInodeIter:
+	cdef ext2_inode_scan scan
+
+cdef class ExtInode:
+	cdef readonly int number
+	cdef ext2_inode inode
---
+++ pyext2lib.pyx
@@ -0,0 +1,81 @@
+from pyext2lib cimport *
+
+IO_MANAGER_UNIX = 1
+
+class ExtException(Exception):
+	pass
+
+cpdef open(name, iomanager, flags = 0, superblock = 0, block_size = 0):
+	cdef ext2_filsys fs
+	cdef io_manager iom
+
+	if iomanager == IO_MANAGER_UNIX:
+		iom = unix_io_manager
+	else:
+		# XXX - Fill in other io managers
+		raise ExtException("Unrecognized IO manager!")
+
+	if ext2fs_open(name, flags, superblock, block_size, iom, &fs):
+		raise ExtException("Can't open filesystem! Are you root?")
+
+	filesystem = ExtFS()
+	filesystem.fs = fs
+
+	return filesystem
+
+cdef class ExtFS:
+	def __dealloc__(self):
+		self.close()
+
+	cpdef read_block_bitmap(self):
+		if ext2fs_read_block_bitmap(self.fs):
+			raise ExtException("Can't read block bitmap!")
+
+	cpdef read_inode_bitmap(self):
+		if ext2fs_read_inode_bitmap(self.fs):
+			raise ExtException("Can't read block bitmap!")
+
+	cpdef read_bitmaps(self):
+		if ext2fs_read_bitmaps(self.fs):
+			raise ExtException("Can't read bitmaps!")
+
+	cpdef flush(self):
+		if ext2fs_flush(self.fs):
+			raise ExtException("Can't flush filesystem!")
+
+	cpdef close(self):
+		if ext2fs_close(self.fs):
+			raise ExtException("Can't close filesystem!")
+
+	cpdef iterinodes(self, skip_missing_itable = False):
+		return ExtFSInodeIter(self, skip_missing_itable)
+
+cdef class ExtFSInodeIter:
+	def __init__(self, ExtFS extfs, skip_missing_itable):
+		if ext2fs_open_inode_scan(extfs.fs, 0, &self.scan):
+			raise ExtException("Can't open inode scan!")
+
+		if skip_missing_itable:
+			ext2fs_inode_scan_flags(self.scan, EXT2_SF_SKIP_MISSING_ITABLE, 0)
+
+	def __dealloc__(self):
+		ext2fs_close_inode_scan(self.scan)
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		ret = ExtInode()
+
+		if ext2fs_get_next_inode(self.scan, &ret.number, &ret.inode):
+			raise ExtException("Can't get next inode!")
+
+		# XXX ext2fs_get_next_inode seems to return 0 as the inode number
+		# long before all inodes are actually traversed.
+		if ret.number == 0:
+			raise StopIteration
+
+		return ret
+
+cdef class ExtInode:
+	pass
