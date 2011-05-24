c775765846ce6a559790a4fd3761da57e828e080
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-05-24 17:04:57 -0300

    Added license.

--- src/pyext2lib.pyx
+++ src/pyext2lib.pyx
@@ -1,3 +1,19 @@
+#	Copyright 2011, Guilherme Gonçalves (guilherme.p.gonc@gmail.com)
+#	This file is part of PyExt2lib.
+#
+#	PyExt2lib is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	PyExt2lib is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with PyExt2lib.  If not, see <http://www.gnu.org/licenses/>.
+
 from math import ceil
 from libc.stdlib cimport *
 
